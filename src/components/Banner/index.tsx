fdf78c76ea3d4a4f6d589b6dbd810ec271917565
Weydson Marinho <weydsonmarinho@gmail.com>
2022-04-04 15:20:44 -0300

    fix: Banner Component responsiveness

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -8,42 +8,39 @@ export function Banner(): JSX.Element {
       backgroundImage="url(images/banner-background.jpg)"
       backgroundRepeat="no-repeat"
       backgroundSize="cover"
+      justify="center"
     >
-      <Flex
-        justify={["center", "space-between"]}
-        align="center"
-        w="100%"
-        mx="auto"
-        px={["4", "10", "15", "20", "36"]}
-      >
-        <Box>
-          <Heading
-            color="gray.100"
-            fontWeight="500"
-            fontSize={["xl", "2l", "2xl", "2xl", "4xl"]}
-            mb="20px"
-          >
-            5 Continentes,
-            <br /> infinitas possibilidades.
-          </Heading>
+      <Flex width="100%" maxW="1160px">
+        <Flex justify={["center", "space-between"]} align="center" w="99%">
+          <Box>
+            <Heading
+              color="gray.100"
+              fontWeight="500"
+              fontSize={["xl", "2l", "2xl", "2xl", "4xl"]}
+              mb="20px"
+            >
+              5 Continentes,
+              <br /> infinitas possibilidades.
+            </Heading>
 
-          <Text
-            color="gray.300"
-            mt="5"
-            fontSize={["0.8rem", "xl"]}
-            maxW={["100%", "100%", "100%", "550px"]}
-          >
-            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
-          </Text>
-        </Box>
-        <Image
-          w={["300px", "300px", "300px", "350px", "430px"]}
-          display={["none", "none", "block"]}
-          transform="rotate(3deg) translateY(48px)"
-          src="images/airplane.svg"
-          ml="8"
-          alt-="Avião amarelo voando com nuvens ao seu redor  "
-        />
+            <Text
+              color="gray.300"
+              mt="5"
+              fontSize={["0.8rem", "xl"]}
+              maxW={["100%", "100%", "100%", "550px"]}
+            >
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+            </Text>
+          </Box>
+          <Image
+            w={["300px", "300px", "300px", "350px", "430px"]}
+            display={["none", "none", "block"]}
+            transform="rotate(3deg) translateY(48px)"
+            src="images/airplane.svg"
+            ml="8"
+            alt-="Avião amarelo voando com nuvens ao seu redor  "
+          />
+        </Flex>
       </Flex>
     </Flex>
   );
