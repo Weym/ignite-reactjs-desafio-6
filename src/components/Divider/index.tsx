948135e38055ec5198214c89813a276cd071ced1
Weydson Marinho <weydsonmarinho@gmail.com>
2022-04-23 15:50:18 -0300

    change: The Heading and the Divider are now on the same Component

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -1,13 +1,26 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading } from "@chakra-ui/react";
 
 export function Divider(): JSX.Element {
   return (
-    <Box
-      width={["60px", "90px"]}
-      height="2px"
-      marginTop={["36px", "80px"]}
-      marginX="auto"
-      bg="gray.700"
-    />
+    <>
+      <Box
+        width={["60px", "90px"]}
+        height="2px"
+        marginTop={["36px", "80px"]}
+        marginX="auto"
+        bg="gray.700"
+      />
+      <Heading
+        marginY={["24px", "52px"]}
+        marginX="auto"
+        fontWeight="500"
+        textAlign="center"
+        fontSize={["lg", "3xl", "4xl"]}
+      >
+        Vamos nessa?
+        <br />
+        Então escolha seu continente
+      </Heading>
+    </>
   );
 }
